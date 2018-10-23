e7404020e271d98a465082bff2cee453d4aeeadc
pacosw1 <pacoswilliams@gmail.com>
2018-10-23 02:53:41 +0000

    added state to component with hard coded data

--- src/App.js
+++ src/App.js
@@ -7,12 +7,42 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./NavBar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          name: "Paco",
+          parts: "10",
+          complexity: "H",
+          estimatedT: "10",
+          testDetail: "Should display in content view"
+        },
+        {
+          name: "Juan",
+          parts: "20",
+          complexity: "E",
+          estimatedT: "1",
+          testDetail: "Should display in content view"
+        }
+      ],
+      currentItem: {
+        name: "Paco",
+        parts: "10",
+        complexity: "H",
+        estimatedT: "10",
+        testDetail: "Should display in content view"
+      }
+    };
+  }
   render() {
+    const items = this.state.items;
+    const curr = this.state.currentItem;
     return (
       <div>
         <NavBar />
-        <ContentDisplay />
-        <SideBar />
+        <ContentDisplay current={curr} />
+        <SideBar items={items} />
       </div>
     );
   }
