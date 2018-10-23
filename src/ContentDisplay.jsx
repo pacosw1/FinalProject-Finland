40251ea376dac7820fedd2fc7057557372a8037c
pacosw1 <pacoswilliams@gmail.com>
2018-10-23 02:57:23 +0000

    added props to component to test display functionality

--- src/ContentDisplay.jsx
+++ src/ContentDisplay.jsx
@@ -2,9 +2,13 @@ import React, { Component } from "react";
 
 class ContentDisplay extends Component {
   render() {
+    const curr = this.props.current;
     return (
       <div className="split right">
-        <h1>Content Display</h1>
+        <h1> </h1>
+        <h1>{curr.name}</h1>
+        <h1>{curr.complexity}</h1>
+        <h1>{curr.testDetail}</h1>
       </div>
     );
   }
