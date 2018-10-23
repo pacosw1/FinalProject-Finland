62ae4845ae0a047c7313a262b23c1c8cd0a8d006
pacosw1 <pacoswilliams@gmail.com>
2018-10-23 06:53:07 +0000

    small changes to implement contentDisplay functionality

--- src/App.js
+++ src/App.js
@@ -12,18 +12,16 @@ class App extends Component {
     this.handleClick = this.handleClick.bind(this);
     this.state = {
       items: itemList,
-      currentItem: {
-        id: "h12jbbj21",
-        name: "Paco",
-        parts: "10",
-        complexity: "H",
-        estimatedT: "10",
-        testDetail: "Should display in content view"
-      }
+      currentItem: itemList[0]
     };
   }
 
   fetchById(id) {
+    const obj = {
+      length: 10,
+      width: 20,
+      thickness: 0.2
+    };
     //this method finds and returns the object with matching id
     return this.state.items.find(item => item.id === id);
   }
@@ -32,15 +30,16 @@ class App extends Component {
     this.setState({ currentItem: this.fetchById(id) });
     //console.log(this.fetchById(id));
   }
+  renderContent() {
+    return <ContentDisplay current={this.state.currentItem} />;
+  }
 
   render() {
-    const items = this.state.items;
-    const curr = this.state.currentItem;
     return (
       <div>
         <NavBar />
-        <ContentDisplay current={curr} />
-        <SideBar handleClick={this.handleClick} items={items} />
+        {this.renderContent()}
+        <SideBar handleClick={this.handleClick} items={this.state.items} />
       </div>
     );
   }
@@ -48,44 +47,105 @@ class App extends Component {
 const itemList = [
   //hard-coded test data
   {
-    id: "h12jbbj21",
-    name: "Paco",
-    parts: "10",
-    complexity: "H",
-    estimatedT: "10",
-    testDetail: "Should display in content view"
-  },
-  {
     id: "h11jbbj21",
     name: "Luke",
     parts: "20",
     complexity: "E",
     estimatedT: "1",
-    testDetail: "Should display in content view"
+    testDetail: "Should display in content view",
+    parts: [
+      {
+        material: "aluminum",
+        picture: "url/",
+        dimensions: {
+          length: 10,
+          width: 20,
+          thickness: 30
+        },
+        features: [
+          {
+            name: "hole",
+            quantity: 10,
+            time: 20
+          }
+        ]
+      }
+    ]
   },
   {
     id: "h12jbbj2L",
     name: "Matthew",
-    parts: "20",
     complexity: "E",
     estimatedT: "1",
-    testDetail: "Should display in content view"
+    testDetail: "Should display in content view",
+    parts: [
+      {
+        material: "aluminum",
+        picture: "url/",
+        dimensions: {
+          length: 10,
+          width: 20,
+          thickness: 30
+        },
+        features: [
+          {
+            name: "hole",
+            quantity: 10,
+            time: 20
+          }
+        ]
+      }
+    ]
   },
   {
     id: "Mm12323",
     name: "John",
-    parts: "20",
     complexity: "E",
     estimatedT: "1",
-    testDetail: "Should display in content view"
+    testDetail: "Should display in content view",
+    parts: [
+      {
+        material: "notALuminum",
+        picture: "url/",
+        dimensions: {
+          length: 10,
+          width: 2,
+          thickness: 30
+        },
+        features: [
+          {
+            name: "xd",
+            quantity: 10,
+            time: 20
+          }
+        ]
+      }
+    ]
   },
   {
     id: "h12jbb3",
     name: "Robert",
-    parts: "20",
     complexity: "E",
     estimatedT: "1",
-    testDetail: "Should display in content view"
+    testDetail: "Should display in content view",
+    parts: [
+      {
+        material: "Null",
+        picture: "url/",
+        dimensions: {
+          length: 10,
+          width: 20,
+          thickness: 30
+        },
+        features: [
+          {
+            name: "Hey",
+            quantity: 10,
+            time: 20
+          }
+        ]
+      }
+    ]
   }
 ];
 
